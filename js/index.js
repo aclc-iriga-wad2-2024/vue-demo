2cca40a23a40a7a4564f3f60f76b1cee9191b791
Arvic S. Babol <kulotsystems@gmail.com>
2025-04-06 12:40:06 +0800

    Create Vue app.

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    },
+});
+
+app.mount('#app');
