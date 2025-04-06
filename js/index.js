03dae37b83ae2c124730f5ad54271cff6e9ed2ef
Arvic S. Babol <kulotsystems@gmail.com>
2025-04-06 14:29:43 +0800

    Implement add to cart.

--- js/index.js
+++ js/index.js
@@ -9,7 +9,50 @@ const app = Vue.createApp({
                     { key: 'checkout', label: 'Checkout' },
                 ],
                 active: 0
-            }
+            },
+
+            products: [
+                {
+                    id   : 1,
+                    name : 'Safeguard',
+                    price: 22,
+                    stocks: 10
+                },
+                {
+                    id   : 2,
+                    name : 'Lucky Me',
+                    price: 12,
+                    stocks: 5
+                },
+                {
+                    id    : 3,
+                    name  : 'Ligo Sardines',
+                    price : 27,
+                    stocks: 10
+                },
+                {
+                    id    : 4,
+                    name  : 'Modess',
+                    price : 5,
+                    stocks: 100
+                },
+                {
+                    id    : 5,
+                    name  : 'Great Taste Black',
+                    price : 2000,
+                    stocks: 1
+                },
+                {
+                    id    : 6,
+                    name  : 'Colgate',
+                    price : 10,
+                    stocks: 13
+                }
+            ],
+
+            cart: [
+
+            ]
         }
     },
 
@@ -49,6 +92,20 @@ const app = Vue.createApp({
          */
         inCheckoutTab() {
             return this.activeTab.key === 'checkout';
+        },
+
+        /**
+         * @computed cartTotal
+         * @description The total amount of cart products.
+         * @returns {number}
+         */
+        cartTotal() {
+            let total = 0;
+            for (let i = 0; i < this.cart.length; i++) {
+                total += this.cart[i].totalPrice;
+            }
+
+            return total;
         }
     },
 
@@ -61,6 +118,67 @@ const app = Vue.createApp({
          */
         activateTab(tabIndex) {
             this.tabs.active = tabIndex;
+        },
+
+        /**
+         * @method addToCart
+         * @description Add the given product object to the cart.
+         * @param product
+         */
+        addToCart(product) {
+            if (product.stocks <= 0) {
+                alert(product.name + " is out of stocks!");
+                return;
+            }
+            // check if the product is already in the cart
+            let foundProduct = null;
+            for (let i = 0; i < this.cart.length; i++) {
+                const cartProduct = this.cart[i];
+                if (cartProduct.id === product.id) {
+                    foundProduct = cartProduct;
+                    break;
+                }
+            }
+
+            // if the product is found, just add the quantity
+            if (foundProduct !== null) {
+                foundProduct.quantity += 1;
+                foundProduct.totalPrice += product.price;
+            }
+
+            // otherwise, add the product to the cart
+            else {
+                this.cart.push({
+                    ...product,
+                    quantity: 1,
+                    totalPrice: product.price
+                });
+            }
+
+            // reduce the product stocks by 1
+            product.stocks = product.stocks - 1;
+        },
+
+        /**
+         * @method removeFromCart
+         * @description Remove the product from cart with the given product index.
+         * @param {number} productIndex
+         */
+        removeFromCart(productIndex) {
+            // reference the cart product
+            const cartProduct = this.cart[productIndex];
+
+            if (confirm("Are you sure to remove '" + cartProduct.name + "' from your cart?")) {
+                // return the stocks of the cart product
+                for (let i = 0; i < this.products.length; i++) {
+                    if (cartProduct.id === this.products[i].id) {
+                        this.products[i].stocks += cartProduct.quantity;
+                    }
+                }
+
+                // remove the cart product
+                this.cart.splice(productIndex, 1);
+            }
         }
     },
 });
